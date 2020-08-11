7389d0d57cd6d96badafef068b7c693095c0db2a
andy nam <naamoonoo.y@gmail.com>
2020-08-11 15:32:03 +0900

    [#1] evn: default next router and link

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+
+const IndexPage = () => (
+	<>
+		<h1> helle bmart - 9</h1>
+		<Link href="/counter">
+			<a>go to counter</a>
+		</Link>
+	</>
+);
+
+export default IndexPage;
