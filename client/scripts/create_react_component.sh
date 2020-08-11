e07839ec6487c3022a7c78148b9dd5b66b478d96
andy nam <naamoonoo.y@gmail.com>
2020-08-11 15:39:07 +0900

    [#1] env: update script with name conformity

--- client/scripts/create_react_component.sh
+++ client/scripts/create_react_component.sh
@@ -16,7 +16,7 @@ DEFAULT_STYLED_COMPONENT="export const Container = styled.div\`\`"
 echo `mkdir src/${FOLDER}/${FILE_NAME}`
 
 # index.ts
-echo `echo "export {default as ${FILE_NAME}} from './${FILE_NAME}${FIRST_CAP}'" > src/${FOLDER}/${FILE_NAME}/index.ts`
+echo `echo "export {default as ${FILE_NAME}} from './${FILE_NAME}'" > src/${FOLDER}/${FILE_NAME}/index.ts`
 
 # react file
 echo `echo "import React from 'react';
@@ -24,13 +24,13 @@ import * as S from './${FILE_NAME}Style';
 
 type Props = {}
 
-const ${FILE_NAME}${FIRST_CAP}: React.FC<Props> = ({} : Props) => {
+const ${FILE_NAME}: React.FC<Props> = ({} : Props) => {
 
 	return <S.Container>${FILE_NAME}</S.Container>;
 };
 
-export default ${FILE_NAME}${FIRST_CAP};
-" > src/${FOLDER}/${FILE_NAME}/${FILE_NAME}${FIRST_CAP}.tsx`
+export default ${FILE_NAME};
+" > src/${FOLDER}/${FILE_NAME}/${FILE_NAME}.tsx`
 
 # styled component file
 echo `echo "import styled from 'styled-components';
