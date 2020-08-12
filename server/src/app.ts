7a1a58957bede0022563a83057241d8c867abf20
andy nam <naamoonoo.y@gmail.com>
2020-08-12 17:31:21 +0900

    [#8] env: split error handling middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,8 +7,8 @@ import path from 'path';
 import cors from 'cors';
 import passport from 'passport';
 
-import router from '@routes';
-import { CustomError } from '@errors/custom-error';
+import router from './routes';
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 
@@ -29,16 +29,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(router);
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof CustomError) {
-    res.status(err.statusCode);
-    res.json(err.message);
-    return;
-  }
-
-  console.error('invalid error type : ', err);
-  res.json('');
-});
+app.use(errorHandler);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
