73e2a24424c744c74c36f09cbfe5b28517b4d002
이찬호 <0407chan@naver.com>
2020-08-14 01:10:56 +0900

    [#11] feat. cart item layout
    Done
    - 레이아웃 잡았습니다
    - style도 기본적인부분 작업했습니다
    
    Todo
    - 수량 변경하는 함수 전달받는 부분 구현해야합니다.
    - 수량 입력 시 안내 문구 수정해야합니다.

---
+++ client/src/components/CartItem/CartItem.test.tsx
@@ -0,0 +1,19 @@
+import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import {CartItem} from './';
+
+describe('CartItem comopoent', () => {
+  it('matches snapshot', () => {
+    const { container } = render(<CartItem />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('shows the elemnts correctly', () => {
+    const utils = render(<CartItem />);
+    // utils.getByText('+');
+
+  });
+});
+
---
+++ client/src/components/CartItem/CartItem.tsx
@@ -0,0 +1,80 @@
+import React, {useState} from 'react';
+import * as S from './CartItemStyle';
+import comma from '../../utils/numberComma';
+
+type Props = {
+	id:number;
+	checked:Boolean;
+	name:string;
+	img:string;
+	base_price:number;
+	discount:number;
+	price:number;
+}
+// type Props = {}
+
+const CartItem: React.FC<Props> = ({id,checked,name,img,base_price,discount,price} : Props) => {
+// const CartItem: React.FC<Props> = ({} : Props) => {
+	// let checked:Boolean = true;
+	// let	name:string=  "아이폰 XS Pro";
+	// let	img:string = "https://i.imgur.com/0vGb1eJ.png";
+	// let	base_price:number = 1200000;
+	// let	discount:number = 10;
+	// let	price:number =  1180000;
+	
+	const [quantity, setQuantity] = useState<number>(3);
+	const [currDisc, setCurrDisc] = useState<number>(0);
+
+	let description:string[] = ["","1개 이하로 선택할 수 없습니다.","해당 상품은 한번에 10개까지 구매할 수 있어요."]
+	
+	const increaseQuantity = () => {
+		if(quantity >= 100) return;
+		setQuantity(quantity + 1);
+	};
+	const decreaseQuantity = () => {
+		if(quantity <= 1) return;
+		setQuantity(quantity - 1);
+	};
+
+	const deleteCartItem = () => {
+		console.log("삭제했다");
+	}
+	
+	return <S.Container>
+		<S.cartTitle>
+			{name}
+		</S.cartTitle>
+		<S.cartHeader>
+			<S.headerNameWrapper>
+				<S.checkboxWrapper htmlFor="cartCheckbox">
+					<S.headerCheckbox id="cartCheckbox" type="checkbox" {...checked && "checked"}></S.headerCheckbox>
+					<S.headerCheckboxMark></S.headerCheckboxMark>
+				</S.checkboxWrapper>
+				<S.headerName>{name}</S.headerName>
+			</S.headerNameWrapper>
+			<S.deleteButton onClick={() => deleteCartItem()}>삭제</S.deleteButton>
+		</S.cartHeader>
+		<S.cartBody>
+			<S.cartImage src={img}></S.cartImage>
+			<S.cartPriceWrapper>
+				<div>
+					<S.cartDiscount>{discount}%</S.cartDiscount>
+					<S.cartPrice> ({comma(price)}원)</S.cartPrice>
+				</div>
+				<div>
+					<S.cartTotalBasePrice>{comma(base_price*quantity)}원</S.cartTotalBasePrice>
+					<S.cartTotalPrice> {comma(price*quantity)}원</S.cartTotalPrice>
+				</div>
+				<S.cartDescription>{description[currDisc]}</S.cartDescription>
+				<S.cartQuantityWrapper>
+					<S.cartQuantityMinus onClick={() => decreaseQuantity()}> - </S.cartQuantityMinus>
+					<S.cartQuantity>{quantity}</S.cartQuantity>
+					<S.cartQuantityPlus onClick={() => increaseQuantity()}> + </S.cartQuantityPlus>
+				</S.cartQuantityWrapper>
+			</S.cartPriceWrapper>
+		</S.cartBody>
+	</S.Container>;
+};
+
+export default CartItem;
+
