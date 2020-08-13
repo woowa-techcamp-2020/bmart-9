a38dab64c52a6d58d7fe1f9f71372489e445b3f1
andy nam <naamoonoo.y@gmail.com>
2020-08-13 14:47:22 +0900

    [#17] style: global style setup temporarily

---
+++ client/src/styles/GlobalStyle.tsx
@@ -0,0 +1,23 @@
+import reset from 'styled-reset';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  * {
+    box-sizing: border-box;
+  }
+  body{
+    font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  input, button {
+    background-color: transparent;
+    border: none;
+    outline: none;
+  }
+`;
+
+export default GlobalStyle;
