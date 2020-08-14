91f64f71c01e7e2f9e2aa7f8deb444ecf6d27530
이찬호 <0407chan@naver.com>
2020-08-14 13:23:03 +0900

    [#12] fix. &&연산수정, col 여부 수정
    - 중복되는 코드 삭제 했습니다
    - undefiend 검사부분 연산자 수정했습니다
    - 코드가 매우 깔끔해졌습니다

--- client/src/components/HorizontalBar/HorizontalBar.tsx
+++ client/src/components/HorizontalBar/HorizontalBar.tsx
@@ -8,23 +8,10 @@ type Props = {
 }
 
 const HorizontalBar: React.FC<Props> = ({start,center,end} : Props) => {
-	console.log("start=",start,"center=",center,"end=",end);
-	let startCol = "1fr";
-	let centerCol = "1fr";
-	let endCol = "1fr";
-	if(start === undefined){
-		startCol = "";
-	}
-	if(center === undefined){
-		centerCol = "";
-	}
-	if(end === undefined){
-		endCol = "";
-	}
-	return <S.Container isStart={startCol} isCenter={centerCol} isEnd={endCol}>
-		{start !== undefined ? <S.Start>{start}</S.Start> : <></>}
-		{center !== undefined ? <S.Center>{center}</S.Center> : <></> }
-		{end !== undefined ? <S.End>{end}</S.End> : <></>}
+	return <S.Container isStart={start ? "1fr":""} isCenter={center ? "1fr":""} isEnd={end ? "1fr":""}>
+		{start !== undefined && <S.Start>{start}</S.Start>}
+		{center !== undefined && <S.Center>{center}</S.Center>}
+		{end !== undefined && <S.End>{end}</S.End>}
 	</S.Container>;
 };
 
