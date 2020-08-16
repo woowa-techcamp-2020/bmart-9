e63797f0eb9004bbe09523edbcf221ce885b119e
jungcome7 <jungcome7@gmail.com>
2020-08-16 23:46:06 +0900

    [#24] feat : 이미지 개수에 따라 동적으로 동작하는 캐러셀 애니메이션 적용

--- client/src/components/Carousel/CarouselStyle.ts
+++ client/src/components/Carousel/CarouselStyle.ts
@@ -1,7 +1,6 @@
 import styled, { keyframes } from 'styled-components';
 import { Children } from 'react';
 
-
 export const Container = styled.div`
   width: 100vw;
   overflow-x: hidden;
@@ -10,30 +9,31 @@ export const Container = styled.div`
 
 type LengthProps = {
   length: number;
-}
+};
 
 const calcSlideInterval = (frameLength: number) => {
   let keyframe = '';
-  for(let i = 0; i < frameLength; i++) {
-    let startPoint = i * (100 / frameLength);
-    let endPoint = startPoint + (50 / frameLength);
-    keyframe += `${startPoint}%, ${endPoint}% {transform: translateX(-${i*100}%)} `
+  for (let i = 0; i < frameLength; i++) {
+    const unitInterval = 100 / ((frameLength - 1) * 2);
+    let startPoint = 2 * i * unitInterval;
+    let endPoint = startPoint + unitInterval;
+    if(i !== frameLength - 1) {
+      keyframe += `${startPoint}%, ${endPoint}% {transform: translateX(-${i * 100}%)}` 
+    } else {
+      keyframe += `100% {transform: translateX(-${i * 100}%)}`
+    }
   }
-  keyframe += `100% {transform: translateX(0)}`
-  console.log(keyframe)
   return keyframe;
-}
+};
 
 const slide = (length: number) => keyframes`
-  ${calcSlideInterval(length + 1 )}
- `
+  ${calcSlideInterval(length + 1)}
+`
 
 export const Img = styled.img<LengthProps>`
   display: inline-block;
-  width:100vw;
-  height:200px;
-  animation: ${props => slide(props.length)} ${props => props.length * 3}s cubic-bezier(0.785, 0.135, 0.15, 0.86) infinite;
-  &:last-child {
-    animation-fill-mode: backwards;
-  }
+  width: 100vw;
+  height: 200px;
+  animation: ${(props) => slide(props.length)} ${(props) => props.length * 3}s
+    cubic-bezier(0.785, 0.135, 0.15, 0.86) infinite;
 `;
