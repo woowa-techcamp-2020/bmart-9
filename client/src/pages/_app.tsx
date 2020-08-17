a46fe1191e9554a08b93d37206f2dcebab023035
andy nam <naamoonoo.y@gmail.com>
2020-08-17 22:18:44 +0900

    [#41] feat: conter context applied as provider

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import GlobalStyle from '../styles/GlobalStyle';
+import { CombineProviderApp } from '../utils/createContext';
+import { CounterProvider } from '../context/CounterContext';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      {CombineProviderApp(CounterProvider)(
+        <>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </>
+      )}
     </>
   );
 };
