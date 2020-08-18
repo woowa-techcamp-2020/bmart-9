b975307eb437e2a06472598db02264a4a0f1a839
andy nam <naamoonoo.y@gmail.com>
2020-08-18 18:53:11 +0900

    [#48] WIP: admin page work in progress

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -3,11 +3,15 @@ import { AppProps } from 'next/app';
 import GlobalStyle from '../styles/GlobalStyle';
 import { CombineProviderApp } from '../utils/createContext';
 import { CounterProvider } from '../context/CounterContext';
+import { CategoryProvider } from '../context/CategoryContext';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      {CombineProviderApp(CounterProvider)(
+      {CombineProviderApp(
+        CounterProvider,
+        CategoryProvider
+      )(
         <>
           <GlobalStyle />
           <Component {...pageProps} />
