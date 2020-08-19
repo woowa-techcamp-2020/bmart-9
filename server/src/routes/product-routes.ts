2bfe4db9d7fde1cca3af5a097ce910725faeb366
이찬호 <0407chan@naver.com>
2020-08-19 17:28:58 +0900

    [#43] fix. product routes validatebody 추가

--- server/src/routes/product-routes.ts
+++ server/src/routes/product-routes.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 import { getAllProduct, getProductById, createProduct, getProductByCategory2Id } from '../service/product-service';
 import { validateBody } from '../middlewares/validate-body';
-import { CreateProductBody } from '../../../shared';
+import { CreateProductBody, GetProductByCategory2IdParams, GetProductByIdParams } from '../../../shared';
 
 const router = Router();
 
 // all product
 router.get('/', getAllProduct);
 
-router.get('/:id', getProductById);
+router.get('/:id', validateBody<GetProductByIdParams>(['id']), getProductById);
 
 // Product update
 // router.put('/', updateProduct);
@@ -16,6 +16,6 @@ router.get('/:id', getProductById);
 // Product create
 router.post('/', validateBody<CreateProductBody>(["name", 'discount', 'image', 'price', 'basePrice', 'stock', 'createdAt', 'updatedAt', 'category2Id']), createProduct);
 
-router.get('/category/:category2_id', getProductByCategory2Id);
+router.get('/category/:category2_id', validateBody<GetProductByCategory2IdParams>(['category2Id']), getProductByCategory2Id);
 
 export default router;
