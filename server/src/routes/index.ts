d3ddde3b3cd63ae40eb9a9520d308c9bc9f0e351
이찬호 <0407chan@naver.com>
2020-08-21 02:16:07 +0900

    [#52] feat. cart api추가

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import authRouter from './auth-routes';
 import categoryRouter from './category-routes';
 import productRouter from './product-routes';
+import cartRouter from './cart-routes';
 
 const router = Router({});
 
 router.use('/api/auth', authRouter);
 router.use('/api/category', categoryRouter);
 router.use('/api/product', productRouter);
+router.use('/api/cart', cartRouter);
 
 export default router;
