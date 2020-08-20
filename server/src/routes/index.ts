38c22be92fdfdceccaedda91f980ce104d657c2e
naamoonoo <naamoonoo.y@gmail.com>
2020-08-20 22:02:37 +0900

    [#61] feat: add /image routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import authRouter from './auth-routes';
 import categoryRouter from './category-routes';
 import productRouter from './product-routes';
+import imageRouter from './image-routes';
 
 const router = Router({});
 
 router.use('/api/auth', authRouter);
 router.use('/api/category', categoryRouter);
 router.use('/api/product', productRouter);
+router.use('/api/image', imageRouter);
 
 export default router;
