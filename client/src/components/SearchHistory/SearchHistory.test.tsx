b81377c0c3a218678b8ad00a49be22773795629b
naamoonoo <naamoonoo.y@gmail.com>
2020-08-22 01:53:50 +0900

    [#71] feat: split search histroy and remove search

--- client/src/components/SearchHistory/SearchHistory.test.tsx
+++ client/src/components/SearchHistory/SearchHistory.test.tsx
@@ -2,16 +2,16 @@ import '@testing-library/jest-dom/extend-expect';
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 
-import {Search} from './';
+import {SearchHistory} from './';
 
-describe('Search comopoent', () => {
+describe('SearchHistory comopoent', () => {
   it('matches snapshot', () => {
-    const { container } = render(<Search />);
+    const { container } = render(<SearchHistory />);
     expect(container).toMatchSnapshot();
   });
 
   it('shows the elemnts correctly', () => {
-    const utils = render(<Search />);
+    const utils = render(<SearchHistory />);
     // utils.getByText('+');
 
   });
