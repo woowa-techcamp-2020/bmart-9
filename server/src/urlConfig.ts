32a316d04d9ebeaee2df6a96739527ea313c57fe
naamoonoo <naamoonoo.y@gmail.com>
2020-08-22 07:59:53 +0900

    [#71] feat: url config updated

--- server/src/urlConfig.ts
+++ server/src/urlConfig.ts
@@ -2,6 +2,6 @@ export const PORT = 3000;
 
 const devUrl = 'http://localhost:';
 // TODO
-const prodUrl = `http://${process.env.DB_HOST}:`;
+const prodUrl = `http://3.35.51.159:`;
 export const baseUrl =
   (process.env.NODE_MODE === 'production' ? prodUrl : devUrl) + PORT;
