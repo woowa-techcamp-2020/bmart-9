5c8c48cf08712ef6f1d73fcd121e6aa5585e5932
naamoonoo <naamoonoo.y@gmail.com>
2020-08-21 11:36:18 +0900

    [#67] feat: get product based on category2_id api

--- shared/product.ts
+++ shared/product.ts
@@ -1,12 +1,15 @@
 export type Product = {
   id: number;
   name: string;
-  image: string;
-  price: number;
   discount: number;
-  basePrice: number;
-  category2Id: number;
+  img: string;
+  base_price: number;
+  price: number;
   stock: number;
+  created_at: string;
+  updated_at: string;
+  category1_id: number;
+  category2_id: number;
 };
 
 export type CreateProductBody = {
