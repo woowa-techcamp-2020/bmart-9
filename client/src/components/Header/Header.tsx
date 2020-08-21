909777d32fae0c686ac9a938290d257c31f9469b
naamoonoo <naamoonoo.y@gmail.com>
2020-08-22 01:47:47 +0900

    [#71] feat: split input bar from header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import Link from 'next/link';
 import * as S from './HeaderStyle';
 import { HorizontalBar } from '../HorizontalBar';
 import { useRouter } from 'next/router';
 import { Hamburger } from '../Hamburger';
 import { useUser } from '../../hooks/useUser';
-import Link from 'next/link';
-
+import { SearchBar } from '../SearchBar';
 
 type Props = {};
 
 const Header: React.FC<Props> = ({}: Props) => {
-  const { push } = useRouter();
+  const { pathname } = useRouter();
   const { isLoggedIn, signOut } = useUser();
+  const inputVisiblePath = ['/', '/search'];
+
   return (
     <S.Container>
       <HorizontalBar
@@ -27,18 +29,16 @@ const Header: React.FC<Props> = ({}: Props) => {
           )
         }
         center={
-          <S.Image
-            onClick={() => push('/')}
-            height="30px"
-            src="https://bmart-9.s3.ap-northeast-2.amazonaws.com/public/bmart-logo.png"
-          />
+          <Link href="/">
+            <S.Image
+              height="30px"
+              src="https://bmart-9.s3.ap-northeast-2.amazonaws.com/public/bmart-logo.png"
+            />
+          </Link>
         }
         end={<Hamburger />}
       />
-      <S.InputWrapper onClick={() => push('/search')}>
-        🔍
-        <S.Input placeholder="B마트 상품을 검색해보세요!" />
-      </S.InputWrapper>
+      {inputVisiblePath.includes(pathname) && <SearchBar />}
     </S.Container>
   );
 };
