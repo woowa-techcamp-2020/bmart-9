d4ece065b98711e43444081aeb299cebd8c4c7e5
이찬호 <0407chan@naver.com>
2020-08-23 21:51:24 +0900

    [#77] feat. updateCheck, setAllCheck, deleteAllChecked api추가

--- client/src/api/cart.ts
+++ client/src/api/cart.ts
@@ -1,5 +1,5 @@
 import { bmart } from './bmart';
-import { Cart, CartQuantity } from '../../../shared';
+import { Cart, CartQuantity, CartCheck } from '../../../shared';
 
 const getAll = async () => {
     const { data } = await bmart.get<Cart[]>(`/cart`);
@@ -13,14 +13,22 @@ const update = async (updatedCart: Cart) => {
 }
 
 const createTestCart = async (id: number) => {
-    const resutl = await bmart.get<Cart>(`/cart/test/${id}`);
-    return resutl;
+    const result = await bmart.get<Cart>(`/cart/test/${id}`);
+    return result;
 }
-
 
 const updateQuantity = async (params: CartQuantity) => {
     const { data } = await bmart.put<Cart>(`/cart/quantity`, params);
+    return data;
+}
 
+const setCheckAll = async (params: CartCheck) => {
+    const { data } = await bmart.put<Cart[]>(`/cart/checkAll`, params);
+    return data;
+}
+
+const updateCheck = async (params: CartCheck) => {
+    const { data } = await bmart.put<Cart>(`/cart/check`, params);
     return data;
 }
 
@@ -28,6 +36,10 @@ const updateQuantity = async (params: CartQuantity) => {
 const deleteCart = async (id: number) => {
     const { data } = await bmart.delete<Cart>(`/cart/${id}`);
 
+    return data;
+}
+const deleteAllChecked = async (userId: number) => {
+    const { data } = await bmart.delete<Cart>(`/cart/CheckAll/${userId}`);
     return data;
 }
 
@@ -41,7 +53,10 @@ export default {
     getAll,
     update,
     deleteCart,
+    deleteAllChecked,
     create,
     updateQuantity,
+    updateCheck,
     createTestCart,
+    setCheckAll,
 }
