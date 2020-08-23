4ecd94a808d549aaa3eec1d39ed70698a537b0a3
이찬호 <0407chan@naver.com>
2020-08-23 21:48:27 +0900

    [#77] feat. 장바구니페이지 기능추가
    - 전체 선택
    - 전체 삭제
    - 디자인 수정

--- client/src/pages/cartPage.tsx
+++ client/src/pages/cartPage.tsx
@@ -2,85 +2,129 @@ import React, { useState, useEffect } from 'react';
 import * as S from '../styles/cartPageStyle';
 import { Cart } from '../../../shared';
 import { useCart } from '../hooks/useCart';
+
 import { CartItem } from '../components/CartItem';
+import { Checkbox } from '../components/Checkbox'
 import { HorizontalBar } from '../components/HorizontalBar';
+
 import comma from '../utils/numberComma';
-import { useRouter } from 'next/router';
+import Link from "next/link"
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const CartPage = () => {
-  const { cartList, setCartList, createTestCart } = useCart();
-  const [allCheck, setAllCheck] = useState(true);
-  const [totalCost, setTotalCost] = useState(0);
-  const [totalCount, setTotalCount] = useState(0);
-  const { push } = useRouter();
-
+  const { cartList,
+    setCartList,
+    createTestCart,
+    cartCost,
+    cartCount,
+    setCheckAll,
+    deleteAllChecked } = useCart();
+  const [allCheck, setAllCheck] = useState<number>(1);
 
   useEffect(() => {
     setCartList();
   }, []);
 
   useEffect(() => {
-    getTotalCost();
-  }, [cartList]);
-
-  const getTotalCost = () => {
-    let tempCost = 0;
-    let tempCount = 0;
-    cartList?.map((item: Cart) => {
-      tempCost += item.price * item.quantity;
-      tempCount += item.quantity;
-    });
-    setTotalCost(tempCost);
-    setTotalCount(tempCount);
-  };
+    setCheckAll(3, allCheck);
+  }, [allCheck]);
 
   return (
     <>
-      <div className="header">
-        <HorizontalBar start="<" center="장바구니" end=" "></HorizontalBar>
-      </div>
-      {cartList && cartList.length > 0 ? (
-        <>
-          <div className="selectWrapper">
-            <HorizontalBar
-              start="ㅁ 전체 선택"
-              end="선택 비우기"
-            ></HorizontalBar>
-          </div>
-          <div className="cartItemWrapper">
-            <HorizontalBar start="일반상품"></HorizontalBar>
-
+      <S.Container>
+        <S.Header>
+          <Link href="/">
+            <S.HeaderButton onClick={() => setCheckAll(3, 1)}>
+              <FontAwesomeIcon icon={faArrowLeft}></FontAwesomeIcon>
+            </S.HeaderButton>
+          </Link>
+          <S.HeaderContents>
+            장바구니
+          </S.HeaderContents>
+        </S.Header>
+        {cartList && cartList.length > 0 ? (
+          <S.BodyContainer>
+            <S.SelectWrapper>
+              <S.SelectCheckbox>
+                <label htmlFor='cartAllCheckbox'>
+                  <S.AllCheckBox
+                    id='cartAllCheckbox'
+                    type="checkbox"
+                    checked={allCheck === 1}
+                    onChange={() => allCheck === 1 ? setAllCheck(0) : setAllCheck(1)}
+                  >
+                  </S.AllCheckBox>
+                  <S.CheckboxContents>전체 선택</S.CheckboxContents>
+                </label>
+              </S.SelectCheckbox>
+              {cartCount("") > 0 ?
+                <S.DeleteAllButton color="main" onClick={() => deleteAllChecked(3)}>선택 비우기</S.DeleteAllButton>
+                :
+                <S.DeleteAllButton color="#ddd" disabled>선택 비우기</S.DeleteAllButton>
+              }
+            </S.SelectWrapper>
+            <S.TitleWrapper>
+              <S.Title>일반상품</S.Title>
+              {/* <S.TitlePaint /> */}
+            </S.TitleWrapper>
             {cartList.map((item: Cart) => {
               return <CartItem key={item.id} {...item}></CartItem>;
             })}
-            <HorizontalBar center={
-              <S.TextButton onClick={() => push(`/`)}>
-                + 더 담으러 가기
-              </S.TextButton>}>
-            </HorizontalBar>
-          </div>
-          <div className="totalWrapper">
+            <S.MoreButtonWrapper>
+              <Link href="/">
+                <S.TextButton onClick={() => setCheckAll(3, 1)}>
+                  + 더 담으러 가기
+                </S.TextButton>
+              </Link>
+            </S.MoreButtonWrapper>
+
             <HorizontalBar
-              center={`주문금액 : ${comma(totalCost)} 원`}
+              center={`주문금액 : ${comma(cartCost())} 원`}
             ></HorizontalBar>
-          </div>
-          <HorizontalBar
-            center={
-              <S.Button>
-                {' '}
-                {`${totalCount}개 ${comma(totalCost)}`}원 배달 주문 하기
-              </S.Button>
-            }
-          ></HorizontalBar>
-        </>
-      ) : (
-          <S.Container>
-            <S.Img src="https://i.imgur.com/t0Lantl.png"></S.Img>
-            <S.Button onClick={() => createTestCart(3)}>
-              테스트 장바구니 추가
-          </S.Button>
-          </S.Container>
-        )}
+            <S.EmptySpace></S.EmptySpace>
+            <HorizontalBar
+              start={
+                cartCount("") > 0 ?
+                  <>
+                    <S.OrderButton>
+                      <S.OrderButtonCount>
+                        {cartCount("")}
+                      </S.OrderButtonCount>
+                      <S.OrderButtonText>
+                        {` ${comma(cartCost())}`}원 배달 주문 하기
+                      </S.OrderButtonText>
+                    </S.OrderButton>
+                    <S.BottomConcealer />
+                  </>
+                  :
+                  <>
+                    <S.EmptyButton>
+                      <S.OrderButtonText>
+                        최소주문금액을 채워주세요
+                    </S.OrderButtonText>
+                    </S.EmptyButton>
+                    <S.BottomConcealer />
+                  </>
+              }
+            ></HorizontalBar>
+          </S.BodyContainer>
+        ) : (
+            <S.EmptyContainer>
+              <S.EmptyWrapper>
+                <S.Img src="https://bmart-9.s3.ap-northeast-2.amazonaws.com/public/empty.png"></S.Img>
+                <div>장바구니가 텅 비어있어요</div>
+              </S.EmptyWrapper>
+              <S.OrderButton onClick={() => createTestCart(3)}>
+                <S.OrderButtonText>
+                  테스트 장바구니 추가
+                </S.OrderButtonText>
+              </S.OrderButton>
+            </S.EmptyContainer>
+          )
+        }
+      </S.Container >
     </>
   );
 };
--- client/src/styles/cartPageStyle.ts
+++ client/src/styles/cartPageStyle.ts
@@ -1,34 +1,203 @@
 import styled from 'styled-components';
+import { MAIN_COLOR1 } from './GlobalStyle';
 
-type barProps = {
-  isStart?: string;
-  isCenter?: string;
-  isEnd?: string;
-};
+const BUTTON_FULL_WIDTH = '96vw';
+const BUTTON_HEIGHT = '6vh';
 
-export const Container = styled.div<barProps>`
-  display: grid;
-  grid-template-rows: 1fr;
-  grid-template-columns: 1fr;
-  height: 94vh;
-  width: 100%;
+const HEADER_HEIGHT = '40px';
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
 `;
+
+// Header
+export const Header = styled.div`
+  display:grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: 1fr auto 1fr;
+  border-bottom: 1px solid #ddd;
+  height: 40px;
+`
+export const HeaderButton = styled.div`
+margin-left: 15px;
+  align-self:center;
+  justify-self: start;
+  color: #444;
+  font-size: large;
+  transition: 0.1s;
+  &:active{
+    font-size: x-large;
+  }
+`
+export const HeaderContents = styled.div`
+  align-self:center;
+  justify-self: center ;
+`
+
+
+// Title
+export const TitleWrapper = styled.div`
+  position:relative;
+`
+
+export const Title = styled.div`
+  margin:15px;
+  font-size:calc(2vh + 2vw);
+  font-weight:bold;
+`
+export const TitlePaint = styled.div`
+  position: absolute;
+  background-color: rgb(42,193,188);
+  left:15px;
+  bottom:-5px;
+  height: 2.5vh;
+  width: 11vh;
+  font-weight: bold;
+  border-radius: 20px;
+`
+
+export const BodyContainer = styled.div`
+  background-color: #eee;
+  overflow-y: scroll;
+  white-space: nowrap;
+  height: calc(100vh - ${HEADER_HEIGHT});
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`
+export const SelectWrapper = styled.div`
+  display:grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: 1fr 1fr;
+  padding:10px;
+  background-color:white;
+`
+
+export const SelectCheckbox = styled.div`
+  justify-self:start;
+  align-self:center;
+`
+
+export const CheckboxContents = styled.span`
+    user-select: none;
+`;
+export const AllCheckBox = styled.input`
+    margin: 5px;
+`;
+
+type buttonProps = {
+  color: string;
+}
+export const DeleteAllButton = styled.button<buttonProps>`
+  justify-self:end;
+  align-self:center;
+  color: ${(props) => props.color === '#ddd' ? props.color : MAIN_COLOR1};
+  transition:0.1s;
+  font-size: calc(1.5vh + 1.5vw);
+`
+
+export const EmptyContainer = styled.div`
+  display:grid;
+  grid: auto auto;
+  height: calc(100vh - ${HEADER_HEIGHT} - 44px - 1.5vh);
+  width: 100%;
+`
+
+export const EmptyWrapper = styled.div`
+  justify-self:center;
+  align-self:center;
+`
 
 export const Img = styled.img`
-  align-self: center;
-  justify-self: center;
   width: 150px;
+  margin-bottom: 20px;
 `;
 
-export const Button = styled.button`
+export const OrderButton = styled.button`
+  position: fixed;
+  justify-self:center;
+  align-self:center;
+  bottom: 1.5vh;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 96vw;
+  height: 6vh;
+  z-index: 10;
+  outline: none;
+  background-color: ${MAIN_COLOR1};
+  cursor: pointer;
+  transition: 0.1s;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.6);
+  &:active{
+    width: 98vw;
+    height: 7vh;
+    span{
+      font-size: calc(1.5vh + 1.5vw);
+    }
+    bottom: 1vh;
+  }
+  border-radius: 5px;
+`;
+
+export const EmptyButton = styled.button`
+  position: fixed;
+  justify-self:center;
+  align-self:center;
+  bottom: 1.5vh;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 96vw;
+  height: 6vh;
+  z-index: 10;
+  font-size: calc(1.4vh + 1.4vw);
+  font-weight: bold;
+  color: white;
+  background-color: #ddd;
+  border-radius: 5px;
+  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.2), 0 4px 7px 0 rgba(0, 0, 0, 0.4);
+`
+export const EmptySpace = styled.div`
+  width:100vw;
+  height:6vh;
+`
+
+export const BottomConcealer = styled.div`
+  position: fixed;
+  bottom:0;
+  left:50%;
+  z-index: 9;
+  transform: translateX(-50%);
+  width:100vw;
+  height:3vh;
+  background-color: #eee;
+`
+
+export const OrderButtonText = styled.span`
+  font-size: calc(1.4vh + 1.4vw);
+  transition: 0.1s;
+  font-weight: bold;
+  color: white;
+`
+export const OrderButtonCount = styled.span`
+  font-size: calc(1.4vh + 1.4vw);
+  transition: 0.1s;
+  font-weight: bold;
+  background-color: white;
+  border-radius: 10px;
+  padding: 3px;
+  color: ${MAIN_COLOR1};
+`
+
+export const TestButton = styled.button`
   align-self: center;
   justify-self: center;
   background-color: green;
   color: white;
   font-weight: bold;
   font-size: large;
-  height: 40px;
-  width: 120%;
+  height: ${BUTTON_HEIGHT};
+  width: ${BUTTON_FULL_WIDTH};
   border-radius: 20px;
   &:hover {
     border: 1px solid green;
@@ -38,15 +207,21 @@ export const Button = styled.button`
   }
 `;
 
+export const MoreButtonWrapper = styled.div`
+  text-align:center;
+`
 export const TextButton = styled.button`
-  background-color: rgba(0,0,0,0);
-  color: green;
-  font-weight: bold;
-  height: 40px;
-  width: 120%;
-  &:hover {
-    border: 1px solid blue;
-    background-color: white;
-    cursor: pointer;
+  width: 96vw;
+  height: 6vh;
+  outline: none;
+  background-color: white;
+  cursor: pointer;
+  transition: 0.1s;
+  color: ${MAIN_COLOR1};
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  font-size: calc(1.3vh + 1.3vw);
+  &:active{
+    font-size: calc(1.5vh + 1.5vw);
   }
 `;
