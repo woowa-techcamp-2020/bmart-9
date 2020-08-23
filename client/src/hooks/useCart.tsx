afd9cb047c222a0a2d2dbe693107c5d855ff06bf
이찬호 <0407chan@naver.com>
2020-08-23 21:49:26 +0900

    [#77] feat. cartCount, cartCost, updateCheck 추가

--- client/src/hooks/useCart.tsx
+++ client/src/hooks/useCart.tsx
@@ -2,37 +2,78 @@ import React, { useEffect } from 'react';
 import { useCreator } from '../utils/createContext';
 import API from '../api';
 import { CartContexts } from '../context/CartContext'
-import { Cart, CartQuantity } from '../../../shared';
+import { Cart, CartQuantity, CartCheck } from '../../../shared';
 
 export const useCart = () => {
   const [cartList, dispatch] = useCreator(CartContexts);
+
+  const cartCount = (from: string) => {
+    let tempCount = 0;
+    if (cartList.length > 0) {
+      cartList.forEach((item: Cart) => {
+        if (item.isCheck === 1) {
+          tempCount += item.quantity;
+        }
+      });
+    }
+    if (from === "cartButton" && tempCount > 99) {
+      return "99+";
+    }
+
+    return tempCount;
+  };
+
+  const cartCost = () => {
+    let tempCost = 0;
+    if (cartList.length > 0) {
+      cartList.forEach((item: Cart) => {
+        if (item.isCheck === 1) {
+          tempCost += item.price * item.quantity;
+        }
+      });
+    }
+    return tempCost;
+  }
 
   const setCartList = async () => {
     const payloadCartList = await API.Cart.getAll();
     dispatch({ type: 'SET_CART_LIST', cartList: payloadCartList });
   };
 
+  const setCheckAll = async (userId: number, isCheck: number) => {
+    const cartParams: CartCheck = { id: userId, isCheck };
+    const payloadCartList = await API.Cart.setCheckAll(cartParams);
+    dispatch({ type: 'SET_CART_LIST', cartList: payloadCartList });
+  };
+
   const updateCartQuantity = async (id: number, quantity: number) => {
-    const cartPrams: CartQuantity = { id, quantity };
-    const payloadCart = await API.Cart.updateQuantity(cartPrams);
+    const cartParams: CartQuantity = { id, quantity };
+    const payloadCart = await API.Cart.updateQuantity(cartParams);
+    dispatch({ type: 'UPDATE_CART_ITEM', udpatedCart: payloadCart });
+  };
+
+  const updateCartCheck = async (id: number, isCheck: number) => {
+    const cartParams: CartCheck = { id, isCheck };
+    const payloadCart = await API.Cart.updateCheck(cartParams);
     dispatch({ type: 'UPDATE_CART_ITEM', udpatedCart: payloadCart });
   };
 
   const createTestCart = async (id: number) => {
-    console.log(id, "이거다!");
     const result = await API.Cart.createTestCart(id);
-
-    const payloadCartList = await API.Cart.getAll();
-    dispatch({ type: 'SET_CART_LIST', cartList: payloadCartList });
+    setCartList();
   };
 
   const deleteCart = async (id: number) => {
     const payloadCart = await API.Cart.deleteCart(id);
-
     dispatch({ type: 'DELETE_CART_ITEM', id });
   }
 
-  return { cartList, setCartList, updateCartQuantity, deleteCart, createTestCart };
+  const deleteAllChecked = async (userId: number) => {
+    const result = await API.Cart.deleteAllChecked(userId);
+    setCartList();
+  }
+
+  return { cartList, updateCartCheck, cartCount, setCheckAll, cartCost, setCartList, updateCartQuantity, deleteCart, deleteAllChecked, createTestCart };
 };
 
 
