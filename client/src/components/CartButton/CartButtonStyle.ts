c68fa98f6a37e546df584141ac5c7cf20cdadc92
이찬호 <0407chan@naver.com>
2020-08-23 21:47:11 +0900

    [#77] feat. 장바구니버튼 디자인수정

--- client/src/components/CartButton/CartButtonStyle.ts
+++ client/src/components/CartButton/CartButtonStyle.ts
@@ -1,24 +1,49 @@
 import styled from 'styled-components';
+import { MAIN_COLOR1, TRANS_TIME } from '../../styles/GlobalStyle';
+
 
 export const Container = styled.div``
 export const Button = styled.button`
     position: fixed;
     bottom: 20px;
     right: 20px;
+    width: 3.5rem;
+    height: 3.5rem;
     z-index: 10;
-    font-size: 18px;
+    font-size: 1.6rem;
     outline: none;
     background-color: rgb(42,193,188);
     color: white;
     cursor: pointer;
     padding: 15px;
     border-radius: 100%;
-    transition: 0.2s;
+    transition: ${TRANS_TIME};
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.6);
-    &:hover{
-        background-color: white;
-        color: rgb(42,193,188);
-        box-shadow: 0 0 8px 2px rgba(42,193,188, 0.2), 0 0 8px 2px rgba(42,193,188, 0.6);
+    
+    &:active{
+        bottom: 17px;
+        right: 17px;
+        width: 4rem;
+        height: 4rem;
+        font-size: 1.8rem;
+        div{
+            font-size: 0.8rem;
+        }
     }
 `
+export const CartCount = styled.div`
+    position: absolute;
+    top: 6px;
+    right: 2px;
+    font-size: 0.7rem;
+    padding: 2px;
+    padding-left:4px;
+    padding-right:4px;
+    font-weight: bold;
+    transition: ${TRANS_TIME};
+    border: solid 2px rgb(42,193,188);
+    border-radius: 20px;
+    color: rgb(42,193,188);
+    background-color: white;
 
+`
