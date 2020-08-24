dbbd893ef3fa5bc05b489c3313fe961d5efcff3f
naamoonoo <naamoonoo.y@gmail.com>
2020-08-24 18:08:39 +0900

    [#86] feat: each category item can redirect

--- client/src/components/BoxCategory/BoxCategory.tsx
+++ client/src/components/BoxCategory/BoxCategory.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import * as S from './BoxCategoryStyle';
 import { Category } from '../../../../shared';
 import { MainContainer } from '../MainContainer';
+import Link from 'next/link';
 
 type Props = {
   categories?: Category[];
@@ -12,7 +13,15 @@ const BoxCategory: React.FC<Props> = ({ categories }: Props) => {
     <MainContainer>
       <S.Container>
         {categories &&
-          categories.map((cate) => <S.Item key={cate.id}>{cate.name}</S.Item>)}
+          categories.map((cate) => (
+            <Link
+              key={cate.id}
+              href="/category/[id]"
+              as={`/category/${cate.id}`}
+            >
+              <S.Item>{cate.name}</S.Item>
+            </Link>
+          ))}
       </S.Container>
     </MainContainer>
   );
