55f74d66c069e4cc9efd232214a8647e6b9ec578
이찬호 <0407chan@naver.com>
2020-08-24 19:40:44 +0900

    [#88] fix. cartContext변경

--- client/src/context/CartContext.tsx
+++ client/src/context/CartContext.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { contextCreator } from '../utils/createContext';
-import { Cart } from '../../../shared'
-import cart from '../api/cart';
+import { Cart, ClientCart } from '../../../shared'
 
-export type clientType = Cart & { checked: boolean };
-
-export type CartState = Array<clientType>;
+export type CartState = ClientCart[];
 
 export type CartAction =
-	| { type: 'SET_CART_LIST'; cartList: clientType[] }
-	| { type: 'UPDATE_CART_ITEM'; udpatedCart: Cart; }
+	| { type: 'SET_CART_LIST'; cartList: Cart[] }
+	| { type: 'SET_CLIENT_CART_LIST'; cartList: ClientCart[] }
+	| { type: 'UPDATE_CART_ITEM'; udpatedCart: ClientCart; }
+	| { type: 'UPDATE_CART_CHECK'; id: number, check: number }
 	| { type: 'DELETE_CART_ITEM'; id: number }
 
 const CartReducer = (
@@ -18,11 +17,15 @@ const CartReducer = (
 ): CartState => {
 	switch (action.type) {
 		case 'SET_CART_LIST':
-			return action.cartList.map((cart) => ({ ...cart, checked: true })); // new state
+			return action.cartList.map((cart) => ({ ...cart, check: 1 })); // new state
+		case 'SET_CLIENT_CART_LIST':
+			return [...action.cartList];
 		case 'UPDATE_CART_ITEM':
-			return state.map((cart) => cart.id === action.udpatedCart.id ? { ...action.udpatedCart, checked: true } : cart);
+			return state.map((clientCart) => clientCart.id === action.udpatedCart.id ? action.udpatedCart : clientCart);
 		case 'DELETE_CART_ITEM':
-			return state.filter((cart) => cart.id !== action.id);
+			return state.filter((clientCart) => clientCart.id !== action.id);
+		// case 'UPDATE_CART_CHECK':
+		// 	return state.map((clientCart) => clientCart.id === action.id && clientCart.check = action.check);
 		default:
 			throw new Error('Unhandled action');
 	}
