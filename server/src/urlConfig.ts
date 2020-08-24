073a4e055967c8b55af8ff457f507705ae040af0
naamoonoo <naamoonoo.y@gmail.com>
2020-08-24 16:51:46 +0900

    [#80] refactor: update base url, split with port

--- server/src/urlConfig.ts
+++ server/src/urlConfig.ts
@@ -1,7 +1,7 @@
 export const PORT = 3000;
-
+export const CLIENT_PORT = 9000;
 const devUrl = 'http://localhost:';
 // TODO
 const prodUrl = `http://3.35.51.159:`;
 export const baseUrl =
-  (process.env.NODE_MODE === 'production' ? prodUrl : devUrl) + PORT;
+  process.env.NODE_MODE === 'production' ? prodUrl : devUrl;
