f369166cf1bc4e4240c249b2945600f5507cf8f6
naamoonoo <naamoonoo.y@gmail.com>
2020-08-24 16:49:20 +0900

    [#80] refactor: split hard coded image url

---
+++ client/src/images.ts
@@ -0,0 +1,10 @@
+export const Images = {
+  GITHUB:
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/1024px-Octicons-mark-github.svg.png',
+  LOG_OUT:
+    'https://cdn1.iconfinder.com/data/icons/essentials-pack/96/logout_close_sign_out_exit_access-512.png',
+  MAIN_LOGO:
+    'https://bmart-9.s3.ap-northeast-2.amazonaws.com/public/bmart-logo.png',
+  EMPTY_CART:
+    'https://bmart-9.s3.ap-northeast-2.amazonaws.com/public/empty.png',
+};
