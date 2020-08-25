be5d7169be9275e422824c60ee14fc93484d52a3
jungcome7 <jungcome7@gmail.com>
2020-08-25 17:25:09 +0900

    [#83] feat : add product content components

--- client/src/components/CardContent/CardContent.tsx
+++ client/src/components/CardContent/CardContent.tsx
@@ -2,11 +2,23 @@ import React from 'react';
 import * as S from './CardContentStyle';
 import { Product } from '../../../../shared';
 
-export const CardContent: React.FC<Product> = ({ name }: Product) => {
+// id baseprice discount name price
+
+export const CardContent: React.FC<Product> = ({
+  id,
+  base_price,
+  name,
+  discount,
+  price,
+}: Product) => {
+
   return (
     <>
       <S.Container>
         <S.ProductName>{name}</S.ProductName>
+        <S.ProductDiscount>{discount}</S.ProductDiscount>
+        <S.ProductBasePrice>{base_price}</S.ProductBasePrice>
+        <S.ProductPrice>{price}</S.ProductPrice>
       </S.Container>
     </>
   );
--- client/src/components/CardContent/CardContentStyle.ts
+++ client/src/components/CardContent/CardContentStyle.ts
@@ -7,6 +7,30 @@ export const Container = styled.div`
 `
 
 export const ProductName = styled.div`
+color:#454545;
+vertical-align: middle;
+font-size: 14px;
+font-weight: bold;
+margin: 0 15px;
+`
+
+export const ProductDiscount = styled.div`
+  color:#454545;
+  vertical-align: middle;
+  font-size: 14px;
+  font-weight: bold;
+  margin: 0 15px;
+`
+
+export const ProductBasePrice = styled.div`
+  color:#454545;
+  vertical-align: middle;
+  font-size: 14px;
+  font-weight: bold;
+  margin: 0 15px;
+`
+
+export const ProductPrice = styled.div`
   color:#454545;
   vertical-align: middle;
   font-size: 14px;
