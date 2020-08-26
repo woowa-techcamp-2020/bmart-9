542e7ba63e694403dfa344dfeedbeaf034d796b1
naamoonoo <naamoonoo.y@gmail.com>
2020-08-26 21:29:37 +0900

    [#100] feat: connect favorite api in clinent

--- client/src/api/favorite.ts
+++ client/src/api/favorite.ts
@@ -1,20 +1,28 @@
 import { bmartAuth } from './bmart';
-import { Favorite, FavoriteProductId } from '../../../shared';
+import { Favorite, FavoriteProductId, Product } from '../../../shared';
 
-const getAll = async (token: string) => {
+const getFavoriteProductId = async (token: string) => {
   const { data } = await bmartAuth(token).get<FavoriteProductId[]>('/favorite');
 
   return data;
 };
 
+const getFavoriteProduct = async (token: string) => {
+  const { data } = await bmartAuth(token).get<Product[]>('/favorite/products');
+
+  return data;
+};
+
 const removeOne = async (token: string, productId: number) => {
-  const { data } = await bmartAuth(token).delete(`/favorite/${productId}`);
+  const { data } = await bmartAuth(token).delete<FavoriteProductId>(
+    `/favorite/${productId}`
+  );
 
   return data;
 };
 
 const addNewFavorite = async (token: string, productId: number) => {
-  const { data } = await bmartAuth(token).post<Favorite>(`/favorite`, {
+  const { data } = await bmartAuth(token).post<FavoriteProductId>(`/favorite`, {
     productId,
   });
 
@@ -22,7 +30,8 @@ const addNewFavorite = async (token: string, productId: number) => {
 };
 
 export default {
-  getAll,
+  getFavoriteProductId,
+  getFavoriteProduct,
   removeOne,
   addNewFavorite,
 };
