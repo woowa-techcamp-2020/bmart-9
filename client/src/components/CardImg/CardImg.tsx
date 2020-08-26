041200d205ad52b0374f99b417239374a2bf3f38
이찬호 <0407chan@naver.com>
2020-08-27 00:08:58 +0900

    [#91] feat. cardImg에서 쇼핑버튼 누르면 구매 활성화

--- client/src/components/CardImg/CardImg.tsx
+++ client/src/components/CardImg/CardImg.tsx
@@ -1,30 +1,36 @@
 import React, { useState } from 'react';
 import * as S from './CardImgStyle';
 import { faHeart, faShoppingBag } from '@fortawesome/free-solid-svg-icons';
+import { Product } from '../../../../shared';
+
+import { useCartAdd } from '../../hooks/useCartAdd';
+
 // import "framework7-icons"
 
 type Props = {
   imgSrc: string;
   viewportWidth: number
+  product: Product;
 };
 
 export const CardImg: React.FC<Props> = (props: Props) => {
-	const { imgSrc, viewportWidth } = props;
-	
-	const [like, setLike] = useState(false);
-  
-	const toggleLike = () => {
-		setLike(!like);
-	};
+  const { imgSrc, viewportWidth, product } = props;
+  const { openCartAdd } = useCartAdd();
+  const [like, setLike] = useState(false);
+
+  const toggleLike = () => {
+    setLike(!like);
+  };
+
 
   return (
     <>
       <S.Container>
         <S.ImgWrapper>
-          <S.Img src={imgSrc} viewportWidth={viewportWidth}/>
+          <S.Img src={imgSrc} viewportWidth={viewportWidth} />
         </S.ImgWrapper>
-        <S.HeartIcon onClick={toggleLike} icon={faHeart} like={like ? 'red' : 'white'}/>
-        <S.ShoppingBagIcon icon={faShoppingBag} />
+        <S.HeartIcon onClick={toggleLike} icon={faHeart} like={like ? 'red' : 'white'} />
+        <S.ShoppingBagIcon icon={faShoppingBag} onClick={() => openCartAdd(product)} />
       </S.Container>
     </>
   );
