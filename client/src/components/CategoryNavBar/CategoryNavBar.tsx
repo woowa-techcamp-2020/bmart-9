ccc1f98eb28abb2ecbe8e5008519c583873aa13f
jungcome7 <jungcome7@gmail.com>
2020-08-26 22:21:17 +0900

    [#27] feat: create CategoryNavBar

---
+++ client/src/components/CategoryNavBar/CategoryNavBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './CategoryNavBarStyle';
+
+type Props = {}
+
+const CategoryNavBar: React.FC<Props> = ({} : Props) => {
+
+	return <S.Container>CategoryNavBar</S.Container>;
+};
+
+export default CategoryNavBar;
+
