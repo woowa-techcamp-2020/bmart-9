efd984678cb15115c2ab648d2200d9a5e118f22c
naamoonoo <naamoonoo.y@gmail.com>
2020-08-26 23:01:57 +0900

    [#100] feat: update url for deploy

--- client/src/api/bmart.ts
+++ client/src/api/bmart.ts
@@ -4,7 +4,7 @@ export const TOKEN_KEY = 'X-Auth';
 export type MethodType = 'GET' | 'POST' | 'DELETE' | 'PATCH' | 'PUT';
 
 const devURL = 'http://localhost:';
-// TODO
+
 const prodURL = 'http://3.35.51.159:';
 const PORT = 3000;
 
@@ -13,13 +13,13 @@ export const baseURL =
 export const apiUrl = baseURL + PORT + '/api';
 
 export const bmart = Axios.create({
-  baseURL: baseURL,
+  baseURL: apiUrl,
   headers: { 'Content-Type': 'application/json' },
 });
 
 export const bmartAuth = (token: string) =>
   Axios.create({
-    baseURL: baseURL,
+    baseURL: apiUrl,
     headers: {
       'Content-Type': 'application/json',
       [TOKEN_KEY]: token,
