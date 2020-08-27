ba4c3d26197e68b3b1a7352cd43d04a94121bc71
jungcome7 <jungcome7@gmail.com>
2020-08-28 01:26:48 +0900

    [#125] feat : create ProductDetail component

---
+++ client/src/components/ProductDetail/ProductDetail.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './ProductDetailStyle';
+
+type Props = {}
+
+const ProductDetail: React.FC<Props> = ({} : Props) => {
+
+	return <S.Container>ProductDetail</S.Container>;
+};
+
+export default ProductDetail;
+
