7923ecae6aaf0c78ade73bf025186febeaba96a7
naamoonoo <naamoonoo.y@gmail.com>
2020-08-27 22:50:29 +0900

    [#116] feat: sepearte order view

---
+++ client/src/pages/order/[id].tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import API from '../../api';
+import { InferGetStaticPropsType, GetStaticPropsContext } from 'next';
+import { Header } from '../../components/Header';
+import { OrderMap } from '../../components/OrderMap';
+
+const COMPANY_LATITUDE = 37.516675;
+const COMPANY_LONGITUDE = 127.113063;
+
+const OrderDetailPage = ({
+  productInfo,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  return (
+    <>
+      <Header title={'주문내역'} />
+      <OrderMap latitude={COMPANY_LATITUDE} longitude={COMPANY_LONGITUDE} />
+    </>
+  );
+};
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  };
+};
+
+export const getStaticProps = async ({ params }: GetStaticPropsContext) => {
+  const productInfo = await API.Product.getOne(Number(params!.id));
+
+  return {
+    props: { productInfo },
+  };
+};
+
+export default OrderDetailPage;
