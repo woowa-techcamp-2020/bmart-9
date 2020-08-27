9192a3fce4a6938758c237df083e6aa3e98575c3
이찬호 <0407chan@naver.com>
2020-08-27 23:31:12 +0900

    [#110] feat. add order types

---
+++ shared/order.ts
@@ -0,0 +1,56 @@
+import {Cart} from './cart'
+export type Order = {
+  id: number;
+  userId: number;
+  userName: string;
+  orderName: string;
+  totalPrice: number;
+  status: "requested" | "dispatched" | "delivered";
+  createdAt: string;
+  longitude:number;
+  latitude:number;
+};
+
+export type OrderProduct = {
+  id: number;
+  img: string;
+  name: string;
+  price: number;
+  quantity: number;
+};
+
+export type OrderDetail = {
+  id: number;
+  userId: number;
+  userName: string;
+  orderId: number;
+  orderName: string;
+  orderProducts: OrderProduct[];
+  totalPrice: number;
+  status: "requested" | "dispatched" | "delivered";
+  createdAt: string;
+  longitude:number;
+  latitude:number;
+};
+
+export type CreateOrderBody = {
+  createdAt: string;
+  latitude: number;
+  longitude: number;
+  cartList: Cart[];
+};
+
+export type CreateOrderDB = {
+  createdAt: string;
+  longitude:number;
+  latitude:number;
+  userId: number;
+  name: string;
+  totalPrice: number;
+};
+
+export type UpdateOrderStatus = {
+  id: number;
+  status: "requested" | "dispatched" | "delivered";
+};
+
