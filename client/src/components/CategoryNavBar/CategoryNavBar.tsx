a38858994a6230a4a8df89717d0326b6195453b7
jungcome7 <jungcome7@gmail.com>
2020-08-27 21:25:28 +0900

    [#27] feat : 카테고리 선택 항목에 따라 네비게이션바 스무스하게 이동시키기

--- client/src/components/CategoryNavBar/CategoryNavBar.tsx
+++ client/src/components/CategoryNavBar/CategoryNavBar.tsx
@@ -19,19 +19,36 @@ const CategoryNavBar: React.FC<CategoryNavBarProps> = ({
   categoryClickHandler,
   setCategoryTab,
 }: CategoryNavBarProps) => {
-
   const categoryElements: HTMLElement[] = [];
+  let containerElement: any;
+
   const slideCategory = () => {
-    
-    categoryElements.forEach(element => {
-      if(element && element.dataset.categoryId === categoryTab) {
+    categoryElements.forEach((element) => {
+
+      if (element && element.dataset.categoryId === categoryTab) {
         element.style.backgroundColor = 'grey';
         element.style.color = 'white';
+        console.log(element);
+        console.log(element.getClientRects()[0].x)
+        if (element.getClientRects()[0].x > 300) {
+          containerElement.scrollTo({
+            top: 0,
+            left: element.offsetLeft - 10,
+            behavior: 'smooth'
+          })
+        } else if (element.getClientRects()[0].x < 0) {
+          containerElement.scrollTo({
+            top: 0,
+            left: element.offsetLeft - 285,
+            behavior: 'smooth'
+          })
+        }
+
       } else {
         element.style.backgroundColor = '';
         element.style.color = 'grey';
       }
-    })
+    });
   };
 
   useEffect(() => {
@@ -40,7 +57,7 @@ const CategoryNavBar: React.FC<CategoryNavBarProps> = ({
 
   return (
     <>
-      <S.Container>
+      <S.Container ref={(element) => (containerElement = element)}>
         {categories
           .sort((a, b) => a.id - b.id)
           .map((category) => (
