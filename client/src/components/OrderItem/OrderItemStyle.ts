12e39354a44e35b8ef6f571a3d9c2c8fa80fff0d
이찬호 <0407chan@naver.com>
2020-08-28 07:18:46 +0900

    [bug] fix. 주문내역 디테일 페이지 디자인 수정

--- client/src/components/OrderItem/OrderItemStyle.ts
+++ client/src/components/OrderItem/OrderItemStyle.ts
@@ -4,49 +4,57 @@ import { GRAY_002, MAIN_COLOR1 } from '../../styles/GlobalStyle';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  margin: 5px 0;
+  margin:10px;
+  background-color: #fff;
+  border-radius: 10px;
 `;
 
 export const Tag = styled.div<{ color: string }>`
+  justify-self: center;
+  align-self: center;
   width: 50px;
   height: 10px;
   background: ${({ color }) => color};
 `;
 
-export const Warpper = styled.div`
-  padding: 10px 0;
-  box-shadow: 0 2px 1.5px -1.5px black;
-`;
-
-export const Padding = styled.div`
-  height: 5px;
-  width: 100%;
-  background: ${GRAY_002};
+export const FirstLine = styled.div`
+  display:grid;
+  grid-template-columns: 5fr 1fr;
+  height: 5vh;
+  column-gap:10px;
+  grid-template-rows: 1fr ;
+  margin: 20px;
 `;
 
 export const Title = styled.div`
-  font-style: normal;
-  font-weight: 700;
-  font-size: 20px;
-  line-height: 30px;
-
+  justify-self: start;
+  align-self: center;
+  font-weight: bold;
+  font-size: 3vh;
+  height: 100%;
   width: 100%;
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
-
-  display: block;
-  align-items: center;
   text-transform: capitalize;
-
-  padding: 0 20px;
 `;
+
 
 export const ButtonContainer = styled.div`
   display: flex;
   width: 100%;
-  padding: 5px 10px;
+  padding:10px;
 
   justify-content: center;
   align-items: center;
 `;
+
+export const DiscWrapper = styled.div`
+  margin-left: 20px;
+  margin-right: 20px;
+`
+
+export const Disc = styled.div`
+  font-size: 2vh;
+  color: rgb(100,100,100);
+`
